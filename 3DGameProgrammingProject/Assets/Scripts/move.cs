874fd19f29d796a21faaf50a4e33bd20714bd889
Berger Daniel <danielberger932@gmail.com>
2023-01-03 22:14:44 +0100

    add trash

--- 3DGameProgrammingProject/Assets/Scripts/move.cs
+++ 3DGameProgrammingProject/Assets/Scripts/move.cs
@@ -1,26 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    float speed = 1000.0f;
-    // Start is called before the first frame update
+    public float speed = 75f;
+    private bool forward;
+    private int score = 0;
     void Start()
     {
-        speed = 5.0f;
+        gameObject.GetComponent<Rigidbody>().freezeRotation = true;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        speed = 75f;
+        forward = true;
+
+        if (Input.GetKey(KeyCode.W) && forward)
         {
-            transform.Translate(Vector3.forward * Time.deltaTime * speed*10);
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(-1 * Vector3.forward * Time.deltaTime * speed*10);
+            transform.Translate(-1 * Vector3.forward * Time.deltaTime * (speed / 2));
         }
         if (Input.GetKey(KeyCode.A))
         {
@@ -29,6 +32,25 @@ public class move : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            speed = 100000f;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            print("hit");
+            forward = false;
+        }
+        if (collision.gameObject.tag == "trash")
+        {
+            Destroy(collision.gameObject);
+            score++;
+            print(score);
         }
     }
 }
